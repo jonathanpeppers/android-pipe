633e7b88a04b305f5f418f5636d17147de9aec9a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-11-09 16:45:26 -0600

    Manual binding hits speed we want
    [DOTNET] |         Method |     Mean |   Error |  StdDev | Allocated |
    [DOTNET] |--------------- |---------:|--------:|--------:|----------:|
    [DOTNET] |   CallWithPipe | 128.9 μs | 1.78 μs | 1.58 μs |     280 B |
    [DOTNET] | CallFromCSharp | 181.9 μs | 2.47 μs | 1.93 μs |     400 B |

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # android-pipe
- A native messaging pipe to call from C#
+
+A native messaging pipe to call from C#
+
+|         Method |     Mean |   Error |  StdDev | Allocated |
+|--------------- |---------:|--------:|--------:|----------:|
+|   CallWithPipe | 128.9 μs | 1.78 μs | 1.58 μs |     280 B |
+| CallFromCSharp | 181.9 μs | 2.47 μs | 1.93 μs |     400 B |
--- csharp/DotNetPipe.cs
+++ csharp/DotNetPipe.cs
@@ -1,6 +1,6 @@
 using Android.Graphics;
 using Android.Views;
-using System.Buffers;
+using Java.Interop;
 using LayoutDirection = Android.Views.LayoutDirection;
 
 namespace Benchmark;
@@ -30,58 +30,76 @@ public class DotNetPipe
         textView.TextDirection = TextDirection.Rtl;
     }
 
-    Java.Lang.Object[] parameters = new Java.Lang.Object[] {
+    readonly Java.Lang.Object[] parameters;
+    readonly IntPtr native_parameters;
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewText,
-        "foo",
+    public DotNetPipe()
+    {
+        parameters = new Java.Lang.Object[] {
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTypeface,
-        Typeface.Monospace,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewText,
+            "foo",
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTextSize,
-        (int)ComplexUnitType.Pt, 12f,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTypeface,
+            Typeface.Monospace,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTextColor,
-        (int)Color.Red,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTextSize,
+            (int)ComplexUnitType.Pt, 12f,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewLetterSpacing,
-        10f,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTextColor,
+            (int)Color.Red,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTextAlignment,
-        (int)TextAlignment.Center,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewLetterSpacing,
+            10f,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewGravity,
-        (int)GravityFlags.Center,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTextAlignment,
+            (int)TextAlignment.Center,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewSingleLine,
-        true,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewGravity,
+            (int)GravityFlags.Center,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewMaxLines,
-        10,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewSingleLine,
+            true,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewEllipsize,
-        Android.Text.TextUtils.TruncateAt.Middle,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewMaxLines,
+            10,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewPadding,
-        1, 2, 3, 4,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewEllipsize,
+            Android.Text.TextUtils.TruncateAt.Middle,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewPaintFlags,
-        (int)PaintFlags.StrikeThruText,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewPadding,
+            1, 2, 3, 4,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewLineSpacing,
-        1f, 2f,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewPaintFlags,
+            (int)PaintFlags.StrikeThruText,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewLayoutDirection,
-        (int)LayoutDirection.Rtl,
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewLineSpacing,
+            1f, 2f,
 
-        Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTextDirection,
-        (int)TextDirection.Rtl,
-    };
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewLayoutDirection,
+            (int)LayoutDirection.Rtl,
+
+            Com.Microsoft.Android.Pipe.DotNetPipe.TextviewTextDirection,
+            (int)TextDirection.Rtl,
+        };
+
+        native_parameters = JNIEnv.NewArray(parameters);
+    }
 
     [Benchmark]
-    public void CallWithPipe()
+    public unsafe void CallWithPipe()
     {
         var textView = new TextView(Application.Context);
-        Com.Microsoft.Android.Pipe.DotNetPipe.Send(textView, parameters);
+        JniArgumentValue* arguments = stackalloc JniArgumentValue[2];
+        arguments[0] = new JniArgumentValue(textView.Handle);
+        arguments[1] = new JniArgumentValue(native_parameters);
+        Send(arguments);
+    }
+
+    static readonly JniPeerMembers Members = new Com.Microsoft.Android.Pipe.DotNetPipe().JniPeerMembers;
+
+    public static unsafe void Send(JniArgumentValue* __args)
+    {
+       Members.StaticMethods.InvokeVoidMethod("Send.(Landroid/widget/TextView;[Ljava/lang/Object;)V", __args);
     }
 }
